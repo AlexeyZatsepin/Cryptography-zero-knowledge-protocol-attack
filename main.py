181b3e927c2086fee7cfc3e66cb10e95a3461de2
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-12-10 18:46:44 +0200

    fix duplicate t issue

--- main.py
+++ main.py
@@ -10,24 +10,21 @@ def attack(mod,s):
     url = "http://asymcryptwebservice.appspot.com/znp/challenge?y=" + str(y)
     response = s.get(url)
     print "Show y: " + y
-    t = random.randint(1, mod)
     z = int(json.loads(response.content)['root'], 16)
     print "Received z: " + format(z, '02X')
-    if t != z:
-        dev = gcd(t + z, mod)
-        if mod % dev == 0:
-            print "yeah mod dividers is "
-            print dev
-            print format(mod / dev, '02X')
-        else:
-            attack(mod)
+    dev = gcd(t + z, mod)
+    if t != z and mod % dev == 0 and dev != 1:
+        print "yeah mod dividers is "
+        print dev
+        print format(mod / dev, '02X')
+    else:
+        attack(mod,s)
 
 
 if __name__ == '__main__':
     url = "http://asymcryptwebservice.appspot.com/znp/serverKey"
     s = requests.session()
     response = s.get(url)
-    print response
     mod = int(json.loads(response.content)['modulus'], 16)
     print "KEY:" + format(mod, '02X')
     attack(mod,s)
